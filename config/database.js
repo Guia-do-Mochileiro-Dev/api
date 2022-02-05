624df7b6053560a50a4c6345a5a19a8fde4610fb
leticiafrontend <leticia.alesilva@outlook.com>
2022-02-05 20:39:56 -0300

    fix: error deply

--- config/database.js
+++ config/database.js
@@ -1,18 +1,22 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi-guia-do-mochileiro-dev'),
-        username: env('DATABASE_USERNAME', 'strapi-guia-do-mochileiro-dev'),
-        password: env('DATABASE_PASSWORD', '1234'),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: "postgres",
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi-guia-do-mochileiro-dev"),
+        username: env("DATABASE_USERNAME", "strapi-guia-do-mochileiro-dev"),
+        password: env("DATABASE_PASSWORD", "1234"),
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
-      options: {}
+      options: {
+        ssl: true,
+      },
     },
   },
 });
